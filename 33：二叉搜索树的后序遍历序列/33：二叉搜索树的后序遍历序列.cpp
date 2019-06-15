dab05753f612bd109f56493521f60a8648f126ab
Chen <choak@qq.com>
2019-06-15 21:57:40 +0800

    0615#2157

---
+++ 33：二叉搜索树的后序遍历序列/33：二叉搜索树的后序遍历序列.cpp
@@ -0,0 +1,58 @@
+﻿// 33：二叉搜索树的后序遍历序列.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	bool VerifySquenceOfBST(vector<int> sequence) {
+		if (sequence.empty())
+			return false;
+		int len = sequence.size();
+		int root = sequence[len - 1];
+		int i = 0;
+		for (; i < len - 1; i++) {//root的左子树
+			if (sequence[i] > root)
+				break;
+		}
+		int j = i;
+		for (; j < len - 1; j++) {//root的右子树中右没有小于根节点的数
+			if (sequence[j] < root)
+				return false;
+		}
+		bool left = true;
+		if (i > 0) {//存在左子树
+			vector<int> left_tree(sequence.begin(), sequence.begin() + i);//注意用迭代器初始化的时候是左闭右开
+			left = VerifySquenceOfBST(left_tree);
+		}
+		bool right = true;
+		if (i < len - 1) {//存在右子树
+			vector<int> right_tree(sequence.begin()+i, sequence.begin()+(len-1));
+			right = VerifySquenceOfBST(right_tree);
+		}
+		return left && right;
+
+	}
+};
+
+int main()
+{
+	vector<int> s{5,7,6,9,11,10,8};
+	cout << Solution().VerifySquenceOfBST(s) << endl;
+    std::cout << "Hello World!\n"; 
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ 34：二叉树中和为某一值的路径/34：二叉树中和为某一值的路径.cpp
@@ -0,0 +1,54 @@
+﻿// 34：二叉树中和为某一值的路径.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+/*
+*输入一颗二叉树的跟节点和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。
+*路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
+*(注意: 在返回值的list中，数组长度大的数组靠前)
+*/
+
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+class Solution {
+public:
+	vector<vector<int> > FindPath(TreeNode* root, int expectNumber) {
+		if (!root)
+			return {};
+		vector<vector<int>> res;
+		vector<int> path;
+		FindPath(root, expectNumber, 0, res, path);
+		return res;
+	}
+private:
+	void FindPath(TreeNode* root, int expectNumber, int curSum, vector<vector<int>> &res, vector<int> &path) {
+		curSum += root->val;
+		path.push_back(root->val);
+		if (curSum == expectNumber && !root->left && !root->right) {
+			res.push_back(path);
+		}
+		if (root->left)
+			FindPath(root->left, expectNumber, curSum, res, path);
+		if (root->right)
+			FindPath(root->right, expectNumber, curSum, res, path);
+
+
+	}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
