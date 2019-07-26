af210e3079519f36e1c1453d45c8e8ecdfbe9bc7
Chen <choak@qq.com>
2019-07-26 09:57:08 +0800

    0706 2/5

--- 27：二叉树的镜像/27：二叉树的镜像.cpp
+++ 27：二叉树的镜像/27：二叉树的镜像.cpp
@@ -1,11 +1,15 @@
 ﻿// 27：二叉树的镜像.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+//https://www.nowcoder.com/practice/564f4c26aa584921bc75623e48ca3011?tpId=13&tqId=11171&tPage=1&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking
 
 #include "pch.h"
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
+
+/*
+操作给定的二叉树，将其变换为源二叉树的镜像。
+*/
 
 struct TreeNode {
 	int val;
@@ -36,14 +40,3 @@ int main()
 {
     std::cout << "Hello World!\n"; 
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
--- 28：对称的二叉树/28：对称的二叉树.cpp
+++ 28：对称的二叉树/28：对称的二叉树.cpp
@@ -1,5 +1,5 @@
 ﻿// 28：对称的二叉树.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+//https://leetcode.com/problems/symmetric-tree/
 
 
 
@@ -7,7 +7,11 @@
 #include "pch.h"
 #include <iostream>
 
-
+/*
+判断二叉树是否镜对称
+*/
+//错误的想法，左右子树均镜像对称，则二叉树镜像对称。
+//正确的想法是父亲的左节点和右节点相同
 struct TreeNode {
 	int val;
 	struct TreeNode *left;
